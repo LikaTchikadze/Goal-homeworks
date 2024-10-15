a3cd8189e102517c4d85da0b21cc5617485456cf
Lika tchikadze <likatchikadze1@gmail.com>
2024-10-15 16:01:42 +0400

    Add files via upload

---
+++ level 2/homework/level 2/home/level 2 homework.py
@@ -0,0 +1,113 @@
+name = ("lika")
+surname=("tchikadze")
+print(name)
+play=("volleyball")
+age=13
+heigh=149.5
+
+
+print(name + " " + surname)
+
+
+print(type(age))
+print(type(heigh))
+print(type(surname))
+print(type(play))
+
+print( "i am" + " " +str(name)+" " + str(surname)+ " i am " + str(heigh)+ " tall" + " and i play " + str(play)+ "ty"+ "_:3   ")
+
+#time for interasting info ;)
+
+animal_facts = [
+    "Octopuses have three hearts.",
+    "A group of flamingos is called a 'flamboyance.'",
+    "Honey never spoils; archaeologists have found pots of honey in ancient Egyptian tombs that are over 3000 years old and still edible.",
+    "Cows have best friends and become stressed when they are separated."
+]
+
+for fact in animal_facts:
+    print(fact)
+
+
+science_facts = [
+    "Water can exist in three states: solid, liquid, and gas.",
+    "The speed of light is approximately 299,792 kilometers per second.",
+    "Electricity travels at the speed of light."
+]
+
+for fact in science_facts:
+    print(fact)
+
+
+
+historical_events = [
+    "In 1969, humans first landed on the Moon.",
+    "The Great Wall of China is over 13,000 miles long.",
+    "The shortest war in history lasted only 38 to 45 minutes, fought between the British Empire and the Sultanate of Zanzibar.",
+    "The first computer was invented in the 1940s and weighed over a ton."
+]
+
+for event in historical_events:
+    print(event)
+
+
+print(789 + 123)
+print(123 + 243)
+print(555 + 444)
+print(44 + 22)
+print(234 + 345)
+print(7899 + 4567)
+print(578 + 786)
+print(500 + 777)
+print(111 + 111)
+print(666 + 898)
+
+
+print(89 - 23)
+print(56 - 6)
+print(56788 - 345)
+print(567 - 23)
+print(789 - 234)
+print(124 - 234)
+print(4567789 - 2345)
+print(3546543 - 7685)
+print(234 - 78)
+print(342 - 12)
+
+
+print(23 * 12)
+print(398 * 89)
+print(99 * 23)
+print(2345 * 33)
+print(88 * 88)
+print(89 * 88)
+print(843 * 54)
+print(89 * 22)
+print(234578 * 6857)
+print(77 * 2)
+
+
+print(234 / 2)
+print(22222 / 2)
+print(234567 / 232)
+print(20 / 5)
+print(9999 / 9)
+print(2345975 / 234)
+print(60 / 3)
+print(555555 / 55)
+print(4534343 / 43)
+print(2222453 / 212)
+
+
+print(235 // 2)
+print(909090 // 90)
+print(20 // 2)
+print(22 // 12)
+print(212121 // 21)
+print(70 // 2)
+print(50 /25)
+print(290 // 60)
+print(50 // 45)
+print(545454 // 23)
+
+
