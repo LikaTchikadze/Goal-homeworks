4785c10f61bd6972b4c6a456c8d8acde3e214eb1
Lika tchikadze <likatchikadze1@gmail.com>
2024-10-22 15:55:40 +0400

    Add files via upload

---
+++ level-5/class/cw/cw.py
@@ -0,0 +1,16 @@
+print(100 - 29)
+print(48 - 12)
+print(12345 - 123)
+
+print (100 + 1000)
+print(9898 + 12)
+print(1111 + 111)
+
+print( 12 * 10)
+print(600 * 10)
+print(999 * 999)
+
+print(100 / 10)
+print(1000 / 100)
+print(33333 / 33)
+
