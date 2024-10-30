075f6e7e0a8256899a531c06260888f75b2f365d
Lika tchikadze <likatchikadze1@gmail.com>
2024-10-30 20:10:22 +0400

    Add files via upload

---
+++ level 5/hw/hw.py
@@ -0,0 +1,83 @@
+#today we will learn bug and debugging
+
+#so bug is an error in software 
+#and debugging is fixing bugs or errors in softwere to ensure it works correctly
+#fun fact so word "bug" in programing became from 1940's in first ever computer a bug flyed in sistem and thats why its called "bug" and "debugging"
+
+#"texture meaning" refers to how strings and textual data are interupted and processed 
+#"numerical meaning" are how we use and understand numbers including integers and floats 
+
+#we need to use coments in codes becouse it helps with explaining whitch code is doing what
+
+print(123 + 123)
+print(44 + 12)
+print(45678 + 12345667)
+print(90 + 23)
+print(78 + 234)
+print(555 + 555)
+print(34543 + 3443)
+print(7878 + 22)
+print(12121212 + 90)
+print(76 + 23)
+print(3333333 + 13)
+print(712 + 59)
+print(222 + 22)
+print(444 + 222)
+print(111111 + 111)
+
+print(2222 - 22)
+print(222 - 12)
+print( 8888 - 121)
+print(9999 - 90)
+print(8888 - 1111)
+print(1999 - 11)
+print( 239 - 39)
+print(8 - 2)
+print( 8822 - 820)
+print(232323 - 23)
+print(33333 - 44)
+print(12 - 10)
+print(121212121112 - 1212)
+print(898 - 89)
+print(90 - 89)
+
+print( 80 * 10)
+print(12121 * 12)
+print(888 * 88)
+print(22 * 8)
+print(11111 * 11)
+print(99999 * 2)
+print(55 * 55)
+print(343434 * 89)
+print(12121212111211111111112 * 2)
+print(7878787878 * 12)
+print(2323 * 23)
+print(989898 * 2)
+print(6776776 * 3343)
+print(34343667 * 66)
+print(10 * 12)
+
+print(2000 / 10)
+print(90 / 9)
+print(20 / 10)
+print(20000 / 100)
+print(80 / 10)
+print(1000 / 10)
+print(9000 / 20)
+print(111111 / 111)
+print(222 / 22)
+print(133 / 33)
+print(121212 / 12)
+print(8989 / 89)
+print(83 / 45)
+print( 1212121211121223 / 12212)
+print(60 / 3)
+print(5656565 / 45)
+
+
+
+
+
+
+
+
