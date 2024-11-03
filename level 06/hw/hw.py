d5730f0bd5ed10cf72b06c9c0dc449dbe90e1d7b
Lika tchikadze <likatchikadze1@gmail.com>
2024-11-03 16:12:43 +0400

    rename

---
+++ level 06/hw/hw.py
@@ -0,0 +1,22 @@
+#Container types in Python: Data structures that can hold multiple items, like lists, dictionaries, tuples, and sets.
+
+#Empty types in Python: Data structures or variables that are defined but contain no data, such as empty lists ([]), dictionaries ({}), tuples (()), or strings ("") and so on
+
+<button>click me - this image is not here it ran away sorry </button>
+
+<!--
+<h1> - hello
+<h2> - hello
+<h3> - hello
+<h4> - hello
+<h5> - hello
+<h6> - hello
+
+img src="https://cdn.wallpersafari.com/52/77/Snd218.jpg"  width ="500"
+
+ <img src="image from internet lesson image.jpeg" alt="" width="500" height="375"> 42
+
+<img src="second image from internet same lesson image.jpg" alt="" width="500" height="375"> -->
+
+
+
