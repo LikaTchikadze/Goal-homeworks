c207a9726668ed856ab2d70769e19276a82352d5
Lika tchikadze <likatchikadze1@gmail.com>
2024-11-03 16:14:08 +0400

    Delete level 3 directory

--- level 3/cw/cw.py
+++
@@ -1,8 +0,0 @@
-favorite_color = "red"
-name = "Lika"
-last_name = "Chikadze"
-favorite_car = "Audi"
-
-
-
-print ( "hi my name is " + (name) + (last_name)+ "my fav color is " + (favorite_color) + "and also my favorite car is" + (favorite_car))
