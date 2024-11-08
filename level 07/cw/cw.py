19fa4604e40b0e6880ef0fe1be4cc0b7c2f89d05
Lika tchikadze <likatchikadze1@gmail.com>
2024-11-08 20:55:40 +0400

    Add files via upload

---
+++ level 07/cw/cw.py
@@ -0,0 +1,15 @@
+# Gather user information
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+age = int(input("Enter your age: "))
+country = input("Enter your country: ")
+city = input("Enter your city: ")
+favorite_car = input("Enter your favorite car: ")
+favorite_color = input("Enter your favorite color: ")
+
+# Build a sentence from the entered values
+
+
+info =((first_name) + {last_name} + aged {age}, lives in {city}, {country}. " "Their favorite car iss a {favorite_color} {favorite_car})
+
+print(info)
