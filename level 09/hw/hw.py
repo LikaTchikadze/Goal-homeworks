73d5153901880c1a470d4051dd6613bf957bf8a7
Lika tchikadze <likatchikadze1@gmail.com>
2024-11-09 15:06:21 +0400

    Add files via upload

---
+++ level 09/hw/hw.py
@@ -0,0 +1,76 @@
+#კონკატენაცია ნიშნავს ორი ან მეტი სტრინგის ერთმანეთთან შეერთებას python-ში 
+
+#1)
+
+a = 20
+b = 30
+
+print (a == b) #a უდრის ბ
+print (a > b ) #a მეტია b
+print (a < b ) #b მეტია a
+print (a <= b)# ნაკლებია ან ტოლია თუ არა
+print (a != b)#არ უდრის
+print (a <= b)#ნაკლები ან ტოლი
+ 
+#2)
+a = 99
+b = 100
+
+print (a > b)
+print (a < b)
+print (a <= b)
+print (a >= b)
+print (a != b)
+print (a == b)
+
+#3)
+
+a = 50
+b = 10
+
+print (a > b)
+print (a < b)
+print (a <= b)
+print (a >= b)
+print (a == b)
+print (a != b)
+
+#4)
+
+a = 999
+b = 99
+
+print (a < b)
+print (a > b)
+print (a <= b)
+print (a >= b)
+print (a != b)
+print (a == b)
+
+#5)
+
+a = 111
+b = 110
+
+print (a > b)
+print (a < b)
+print (a <= b)
+print (a >= b)
+print (a == b)
+print (a != b)
+
+
+#6)
+
+name = input ('enter your name:')
+last_name = input ('enter your last name:')
+age = input ('enter your age:')
+fav_color = input ('enter your favorite color:') 
+fav_car = input (' enter your favorite car:')
+
+
+print ('hello my name is ' + name + ' ' + 'my last name is ' + last_name + ' i am  ' + age  + ' my favorite color  is ' + fav_color + ' my dream car is ' + fav_car )
+ 
+password = input ('create password:')
+user_name = input (' create your username:')
+
